36db47bf4186f59d42e619c49686a4a1feec4300
radixxx <radov.2011@gmail.com>
2018-04-25 21:40:24 +0300

    Create simple app for using UDP Protocol and java.net Client/Server

---
+++ src/ClientUdpMulticast.java
@@ -0,0 +1,43 @@
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class ClientUdpMulticast {
+
+    private static InetAddress address;
+    private static byte[] buffer;
+    private static DatagramPacket packet;
+    private static String str;
+    private static MulticastSocket socket;
+
+
+    public static void main(String arg[]) throws Exception {
+        System.out.println("Ожидание сообщения от сервера");
+        try {
+// Создание объекта MulticastSocket для получения
+// данных от группы, используя номер порта 1502
+            socket = new MulticastSocket(1502);
+            address = InetAddress.getByName("233.0.0.1");
+// Регистрация клиента в группе
+            socket.joinGroup(address);
+            while (true) {
+                buffer = new byte[256];
+                packet = new DatagramPacket(buffer, buffer.length);
+
+                //Получение данных от сервера
+                socket.receive(packet);
+                str = new String(packet.getData());
+                System.out.println("Полученно сообщение: " + str.trim());
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.leaveGroup(address);
+                socket.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
